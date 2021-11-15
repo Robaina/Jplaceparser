b33e25d097e3a4c638372a74d94c7ab842d72d01
Semidán Robaina Estévez <hello@semidanrobaina.com>
2021-11-15 15:59:08 +0000

    Create jplaceparser.py

---
+++ jplaceparser.py
@@ -0,0 +1,119 @@
+
+import re 
+import json 
+from io import StringIO
+
+from Bio import Phylo 
+
+
+class JplaceParser():
+    """
+    Methods to parse jplace files, as specified in 
+    https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0031009
+    """
+    def __init__(self, path_to_jplace: str) -> None:
+        with open(path_to_jplace, 'r') as JSON:
+            jplace = json.load(JSON)
+        self.jplace = jplace
+    
+    @property
+    def meta(self):
+        """
+        Print metadata
+        """
+        return self.jplace['metadata']
+
+    @property
+    def fields(self): 
+        """
+        Print data fields
+        """
+        return self.jplace['fields']
+
+    @property
+    def tree(self, newick=False):
+        """
+        Return tree in original or newick format
+        Original format contains branch labels
+        in curly brackets. Newick format removes
+        these labels.
+        """
+        if newick:
+            return self.newickfyTree(self.jplace['tree'])
+        else:
+            return self.jplace['tree']
+    
+    @property
+    def placements(self):
+        """
+        Return placement objects
+        """
+        return self.jplace['placements']
+    
+    @staticmethod
+    def newickfyTree(tree_str: str) -> str:
+        """
+        Remove branch IDs from jplace tree string
+        """
+        subs_tree = re.sub("\{(\d+)\}", '', tree_str)
+        return next(Phylo.parse(StringIO(subs_tree), 'newick'))
+    
+    def buildBranchDict(self) -> dict:
+        """
+        Build dictionary with edge/branch numbers as keys and 
+        reference tree leaves as values
+        """
+        def get_id(s):
+            return int(re.search("\{(\d+)\}", s).group(1))
+        
+        original_tree = self.jplace['tree']
+        tree = self.newickfyTree(original_tree)
+        leaves = tree.get_terminals()
+
+        branches = {
+            get_id(original_tree[original_tree.find(leaf.name):]): leaf.name
+            for leaf in leaves
+        }
+        return branches
+
+    def extractPlacementFields(self, pfielddata: list) -> dict:
+        """
+        Get dict with placement field values from list of values
+        """
+        fields = self.jplace['fields']
+        return {field: pfielddata[i] for i, field in enumerate(fields)}
+
+    def selectBestPlacement(self, placement_object: dict) -> dict:
+        """
+        Select placement with lowest likelihood
+        """
+        pdata = [
+            self.extractPlacementFields(pfielddata)
+            for pfielddata in placement_object['p']
+            ]
+        lowest_like_placement = sorted(pdata, key=lambda x: x['likelihood'])[0]
+        return {'p': lowest_like_placement, 'n': placement_object['n']}
+
+    def selectBestPlacements(self):
+        """
+        Select placement with lowest likelihood for 
+        all placement objects in placements
+        """
+        best_placements = [
+            self.selectBestPlacement(placement)
+            for placement in self.jplace['placements']
+        ]
+        return best_placements
+
+    def labelPlacedQueries(self):
+        """
+        Assign reference label to best query placements
+        """
+        ref_dict = self.buildBranchDict()
+        best_placements = self.selectBestPlacements()
+        labeled_placements = {
+            place_object['n'][0]: ref_dict[place_object['p']['edge_num']]
+            for place_object in best_placements
+            if place_object['p']['edge_num'] in ref_dict.keys()
+        }
+        return labeled_placements
